8f9f3d43e5ad38bce8de8b00eb32884d42cf38c7
bangdasun <bangdasun94@gmail.com>
2018-07-07 19:09:12 -0400

    add problem sequential number

--- Algorithms/easy-SingleNumber.py
+++
@@ -1,7 +0,0 @@
-# 1. Math: from the sum
-def singleNumber(array):
-    return 2 * sum(set(array)) - sum(array)
-
-singleNumber([1, 1, 2, 2, 3, 4, 3, 6, 6, -1, -2, -3, -2, -1, -3])
-
-# 2. Hashtable: .pop() method, I didn't found .pop() in Java HashMap =.=
